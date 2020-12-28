e236624362050cd4fd634e7240aabaf1b49d8434
张志强 <zhangzhiqiang21434@talkweb.com.cn>
2020-12-28 11:35:19 +0800

    初始化 gh-pages

---
+++ packages/test/test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="zm-test" :style="{backgroundColor: bgColor}"></div>
+</template>
+
+<script>
+    export default {
+        name: 'ZmTest',
+        props: {
+            bgColor: {
+                type: String,
+                default: "#ccc"
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    .zm-test {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        animation: zm-opacity 2s infinite linear;
+    }
+</style>
